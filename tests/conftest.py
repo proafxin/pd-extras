d19707bdf1c41d4ccd0a8ee5ad9412a6b18f8f77
Masum <billalmasum93@gmail.com>
2023-02-07 20:48:21 +0600

    added tests for column selection and checking

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+"""Specify variables for all tests"""
+
+from io import StringIO
+
+import pandas as pd
+import pytest
+import requests
+
+
+@pytest.fixture(scope="function")
+def data() -> pd.DataFrame:
+    """Use this data for all the tests
+
+    :return: Pandas dataframe of cities.
+    :rtype: ```pd.DataFrame```
+    """
+
+    url = "https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv"
+    response: requests.Response = requests.get(url, timeout=20)
+
+    assert response.status_code == requests.codes.ok
+    csv_data = pd.read_csv(StringIO(response.content.decode()))
+
+    return csv_data

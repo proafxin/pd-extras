a6cd4733074e14f411eb860362fde0d6693c1501
Masum <billalmasum93@gmail.com>
2023-02-15 03:52:50 +0600

    changed doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinxdoc"
+html_theme = "alabaster"
 
-html_theme_options = {"sidebarwidth": 800}
+html_theme_options = {}
 
 html_static_path = []
